566766934a8850a29fc21f5d731252d8db12de4d
Gabriel Alves <Gabriel Alves@Gabriel>
2015-04-02 14:32:24 -0300

    teste

---
+++ HospitalE3G/src/hospitale3g/HospitalE3G.java
@@ -0,0 +1,10 @@
+package hospitale3g;
+
+public class HospitalE3G {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Paiva gay");
+    }
+    
+}
